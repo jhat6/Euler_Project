c0b4fcde62784ee130aebbfa33e6574ef5b86563
JHAT <60554506+jhat6@users.noreply.github.com>
2021-01-30 07:52:33 -0500

    Add files via upload

---
+++ euler28.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 09:09:15 2020
+
+@author: jeffa
+
+Number spiral diagonals
+
+Project Euler Problem 28
+
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+
+"""
+
+
+d = 1001 # spiral dimensions
+
+x = 1 # top left to bottom right diagonal elements including 1
+sum = 0
+for k in range(0, d):
+    x += 2*k
+    #print(x)
+    sum += x
+    
+print("\n")    
+y = 1 # bottom left to top right diagonal elements 
+for j in range(3, d+2, 2):   # begin calculation of y's excluding 1
+    y += 2*(j-1)
+    sum += y
+    #print(y)
+    y += 2*(j-1)
+    sum += y
+    #print(y)
+
+        
+print(sum)
